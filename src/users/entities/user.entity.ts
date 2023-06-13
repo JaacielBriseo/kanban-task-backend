0df867be6abc61bafaa2084aab9b9620a92198fe
JaacielBriseo <jaacielbris23@gmail.com>
2023-06-13 12:27:08 -0700

    feat: user entity and dto

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,33 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'users' })
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @Column('text')
+  @Field(() => String)
+  fullName: string;
+
+  @Column('text')
+  @Field(() => String)
+  email: string;
+
+  @Column('text')
+  @Field(() => String)
+  password: string;
+
+  @Column({
+    type: 'boolean',
+    default: true,
+  })
+  @Field(() => Boolean)
+  isActive: boolean;
+
+  //? Boards relation
+  // @OneToMany(() => Board, (board) => board.user, { lazy: true })
+  // boards: Board[];
 }
